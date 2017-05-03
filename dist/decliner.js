a2dafe239e452b595349ad99b2eef5c34631dc01
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2017-05-03 21:30:31 +0300

    Add error handle construction

--- dist/decliner.js
+++ dist/decliner.js
@@ -3,6 +3,21 @@
     const arrObject = Array;
 
     arrObject.prototype.decline = function(num) {
+        const arr = this;
+        var result = '';
 
+        try {
+            if (arr.length !== 3) throw new SyntaxError('array must have 3 elements');
+
+            arr.forEach(function(el, i) {
+                if (typeof(el) !== "string") throw new TypeError('element #' + (i + 1) + " is not a string in array");
+            });
+
+            if (typeof(num) !== "number") throw new TypeError('decline() argument is not a number');
+        } catch (e) {
+            console.error('decliner.js error: ' + e.name + ' - ' + e.message);
+        }
+
+        return result;
     }
 })();

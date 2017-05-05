e475b6570be4980c4b906243d728b7a42210eaae
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2017-05-05 23:11:26 +0300

    Clean Up

--- dist/decliner.js
+++ dist/decliner.js
@@ -1,23 +1,23 @@
 ;(function () {
-    'use strict';
-    const arrObject = Array;
+	'use strict';
+	const arrObject = Array;
 
-    arrObject.prototype.decline = function(num) {
-        const arr = this;
-        var result = '';
+	arrObject.prototype.decline = function(num) {
+		const arr = this;
+		var result = '';
 
-        try {
-            if (arr.length !== 3) throw new SyntaxError('array must have 3 elements');
+		try {
+			if (arr.length !== 3) throw new SyntaxError('array must have 3 elements');
 
-            arr.forEach(function(el, i) {
-                if (typeof(el) !== "string") throw new TypeError('element #' + (i + 1) + " is not a string in array");
-            });
+			arr.forEach(function(el, i) {
+				if (typeof(el) !== "string") throw new TypeError('element #' + (i + 1) + " is not a string in array");
+			});
 
-            if (typeof(num) !== "number") throw new TypeError('decline() argument is not a number');
-        } catch (e) {
-            console.error('decliner.js error: ' + e.name + ' - ' + e.message);
-        }
+			if (typeof(num) !== "number") throw new TypeError('decline() argument is not a number');
+		} catch (e) {
+			console.error('decliner.js error: ' + e.name + ' - ' + e.message);
+		}
 
-        return result;
-    }
+		return result;
+	}
 })();

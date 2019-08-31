aa4a26229ea60850773bba96f95e6b9f496caef6
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2019-08-31 13:17:10 +0300

    Some minor changes

--- dist/decliner.js
+++ dist/decliner.js
@@ -1,19 +1,18 @@
 ;(function () {
 	'use strict';
-	const arrObject = Array;
 
-	arrObject.prototype.decline = function(num, insert) {
+	Array.prototype.decline = function (num, insert) {
 		const optionsArray = this;
 		const absNum = Math.abs(num);
 		const cases = [2, 0, 1, 1, 1, 2];
-		var result = '';
+		let result = '';
 
 		insert = insert || false;
 
 		try {
 			if (optionsArray.length !== 3) throw new SyntaxError('array must have 3 elements');
 
-			optionsArray.forEach(function(el, i) {
+			optionsArray.map((el, i) => {
 				if (typeof(el) !== 'string') throw new TypeError('element #' + (i + 1) + ' is not a string in array [' + optionsArray + ']');
 			});
 
@@ -21,13 +20,13 @@
 
 			if (typeof(insert) !== 'boolean') throw new TypeError('second argument in decline() should be boolean');
 
-			result = optionsArray[ (absNum % 1 !== 0) ? 1 : (absNum % 100 > 4 && absNum % 100 < 20) ? 2 : cases[(absNum % 10 < 5) ? absNum % 10 : 5] ];
+			result = optionsArray[(absNum % 1 !== 0) ? 1 : (absNum % 100 > 4 && absNum % 100 < 20) ? 2 : cases[(absNum % 10 < 5) ? absNum % 10 : 5]];
+	
+			if (insert === true) result = `${num} ${result}`;
+	
+			return result;
 		} catch (e) {
-			console.error('decliner.js error: ' + e.name + ' - ' + e.message);
+			console.error(`decliner.js error: ${e.name} - ${e.message}`);
 		}
-
-		if (insert === true) result = num + ' ' + result;
-
-		return result;
 	}
 })();
